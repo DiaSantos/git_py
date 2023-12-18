8a660abfc2c48fe356bb216ee037662b83bb348d
ART <diasantos.tecno@gmail.com>
2023-12-18 19:19:12 -0300

    Co-authored-by: ART <diasantos.tecno@gmail.com>
    undefined
    
    undefined

---
+++ calaculadora_basica.py
@@ -0,0 +1,33 @@
+import os
+import time
+
+cls = lambda : os.system('cls')
+
+def calc():
+    while True:
+        op =  input('Por favor selecione a operação dejejada: \n + para soma;\n - para subtração;\n * para multipicação; \n /para divisão;\n R:  ')
+
+        numb1 = float(input("Insisira um numero: "))
+        numb2 = float(input("Insisira um numero: "))
+
+        if op ==  '+':
+            print("{} + {}".format(numb1,numb2))
+            print(numb1 + numb2)
+        elif op ==  '-':
+            print("{} - {}".format(numb1,numb2))
+            print(numb1 - numb2)
+        elif op ==  '*':
+            print("{} * {}".format(numb1,numb2))
+            print(numb1 * numb2)
+        elif op ==  '/':
+            print("{} / {}".format(numb1,numb2))
+            print(numb1 / numb2)
+        else:
+            print("Operação inválida...")
+            continue
+        novamente = input("Deseja realizar outro cálculo? (s/n): ")
+        if novamente.lower() != 's':
+            break
+if __name__ =="__main__":
+    calc()
+cls()
